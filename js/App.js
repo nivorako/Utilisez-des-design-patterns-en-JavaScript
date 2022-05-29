d85886a11c2679cf13d4b2d28900d8e494f4fce7
nivorako <nivo.rakoto@yahoo.fr>
2022-05-29 13:44:54 +0200

    decorator pattern

--- js/App.js
+++ js/App.js
@@ -24,7 +24,7 @@ class App {
         Filter.render()
 
         FullMovies.forEach(movie => {
-                const Template = new MovieCard(movie)
+                const Template = movieCardWithPlayer(new MovieCard(movie))
                 this.$moviesWrapper.appendChild(
                     Template.createMovieCard()
                 )
--- js/decorators/movieCardWithPlayer.js
+++ js/decorators/movieCardWithPlayer.js
@@ -1,2 +1,9 @@
 function movieCardWithPlayer(movieCard) {
+    if(movieCard.movie.actor === "sylvester"){
+        movieCard.$wrapper.addEventListener("click", () => {
+            const player = new PlayerModal(movieCard.movie)
+            player.render()
+        })
+    }
+    return movieCard
 }
